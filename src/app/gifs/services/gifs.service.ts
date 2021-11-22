4cd955cc337f5ae35eda0012773677be29856397
Jorge Burgos <burgosjorge2907@gmail.com>
2021-11-21 22:47:51 -0500

    gifs en pantalla

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,6 +10,9 @@ export class GifsService {
   private _historial: string[] = [];
   private apKey: string = "OMDgceSIIXn0RulJXcoeYBeoRrT6U6RP";
 
+  //TODO: CAMBIAR ANY POR SU TIPO CORRESPONDIENTE
+  public resultados: any[] = [];
+
   constructor( private http: HttpClient ){}
 
   get historial(){
@@ -28,9 +31,10 @@ export class GifsService {
 
     console.log(this._historial);
 
-    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=OMDgceSIIXn0RulJXcoeYBeoRrT6U6RP&q=dragon ball z&limit=10')
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=OMDgceSIIXn0RulJXcoeYBeoRrT6U6RP&q=${ query }&limit=10`)
     .subscribe( (resp: any) => {
       console.log(resp.data);
+      this.resultados = resp.data;
     } )
   }
 
